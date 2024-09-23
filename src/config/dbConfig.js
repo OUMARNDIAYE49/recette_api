58cccb02bba3a489da3ff39a38f721abe3843682
Mohamed Abdallahi M'khaitir <msidi0415@gmail.com>
2024-09-23 09:21:18 +0000

    Add Readme file

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "",
+  password: "rELAX2024",
   database: "recette_api",
 });
 
