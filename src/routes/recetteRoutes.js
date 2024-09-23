a3153a63fdbd8acaa89f34d4b6247015fa72e972
Mohamed Abdallahi M'khaitir <msidi0415@gmail.com>
2024-09-23 23:09:37 +0000

    Modifie recetteRoutes

--- src/routes/recetteRoutes.js
+++ src/routes/recetteRoutes.js
@@ -6,13 +6,14 @@ import {
   updateRecetteController,
   deleteRecetteController,
 } from "../controllers/recetteController.js";
+import { validateRecette } from "../validators/recetteValidator.js"; // Assurez-vous d'importer le validateur
 
 const router = express.Router();
 
 router.get("/recettes", getAllRecettesController);
 router.get("/recettes/:id", getRecetteByIdController);
-router.post("/recettes", createRecetteController);
-router.put("/recettes/:id", updateRecetteController);
+router.post("/recettes", validateRecette, createRecetteController); // Ajout de la validation
+router.put("/recettes/:id", validateRecette, updateRecetteController); // Ajout de la validation
 router.delete("/recettes/:id", deleteRecetteController);
 
 export default router;
