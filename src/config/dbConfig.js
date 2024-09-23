12f8bc2a625c81401c47d11cefbddf020af4065d
OUMARNDIAYE49 <oumar.ndiaye4952@gmail.com>
2024-09-23 09:09:49 +0000

    update dbConfig

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -5,9 +5,9 @@ import mysql from'mysql2/promise';
 const connPool = mysql.createPool({
   host: 'localhost',       // Remplace par ton host si nécessaire
   user: 'root',            // Remplace par ton utilisateur
-  password: '',            // Remplace par ton mot de passe
+  password: 'Nd49523097',            // Remplace par ton mot de passe
   database: 'recette_api'   // Remplace par le nom de ta base de données
-  
+ 
 });
 
 connPool.getConnection().then(() => {
