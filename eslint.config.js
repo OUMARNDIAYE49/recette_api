a13f0f02b7866597fcd770a776b0bb034f903b0c
Mohamed Abdallahi M'khaitir <msidi0415@gmail.com>
2024-09-24 00:16:26 +0000

    Add eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginVue from "eslint-plugin-vue";
+
+export default [
+  {
+    files: ["**/*.{js,mjs,cjs,vue}"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        jasmine: "readonly",
+        describe: "readonly",
+        beforeAll: "readonly",
+        afterAll: "readonly",
+        it: "readonly",
+        expect: "readonly",
+        process: "readonly",
+      },
+    },
+  },
+  pluginJs.configs.recommended,
+  ...pluginVue.configs["flat/essential"],
+];
